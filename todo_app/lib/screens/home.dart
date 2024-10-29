bb6238e4ddc4d6d890ded943abc770f49a2ff7cc
Pranav P K <pranavpk1519@gmail.com>
2024-10-29 14:53:33 +0530

    search bar created

--- todo_app/lib/screens/home.dart
+++ todo_app/lib/screens/home.dart
@@ -10,7 +10,12 @@ class Home extends StatelessWidget {
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
       body: Container(
-        child: Text('This is the home screen'),
+        padding: EdgeInsets.symmetric(horizontal: 20,vertical: 15),
+        child: Column(
+          children: [
+            searchBox(),
+          ],
+        ),
       ),
     );
   }
@@ -35,6 +40,40 @@ class Home extends StatelessWidget {
             ),
           )
         ]),
+    );
+  }
+}
+
+class searchBox extends StatelessWidget {
+  const searchBox({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20)
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: tdBlack,
+            size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(
+            maxHeight: 20,
+            minWidth: 25,
+          ),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(color: tdGrey)
+        ),
+      ),
     );
   }
 }
